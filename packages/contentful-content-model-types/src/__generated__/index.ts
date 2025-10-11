27073cd6a4246741177a440899e11f0cc1a1d395
cysp <742696+cysp@users.noreply.github.com>
2025-10-11 04:19:55 +0000

    feat(data): export content model

--- packages/contentful-content-model-types/src/__generated__/index.ts
+++ packages/contentful-content-model-types/src/__generated__/index.ts
@@ -4,5 +4,3 @@ export { isTypeMetadatatest } from "./TypeMetadatatest";
 export type { TypeMetadatatest, TypeMetadatatestFields, TypeMetadatatestSkeleton } from "./TypeMetadatatest";
 export { isTypePage } from "./TypePage";
 export type { TypePage, TypePageFields, TypePageSkeleton } from "./TypePage";
-export { isTypeTest_page_12345 } from "./TypeTest_page_12345";
-export type { TypeTest_page_12345, TypeTest_page_12345Fields, TypeTest_page_12345Skeleton } from "./TypeTest_page_12345";
